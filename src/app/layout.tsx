1789546d3496fc2aeb595f8c9c7b491fa9ed569e
Nurseiit Sharip <nurseit.js@gmail.com>
2025-10-21 11:00:08 +0500

    Change default theme in RootLayout from 'system' to 'dark'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -119,7 +119,7 @@ export default function RootLayout({
       <body className={`${inter.variable} ${poppins.variable} font-sans antialiased`} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
