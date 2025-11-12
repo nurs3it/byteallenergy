c50a2650f98ccede5d872ea8fed6cacc41cc99fa
Nurseiit Sharip <nurseit.js@gmail.com>
2025-11-13 01:02:55 +0500

    Remove 'out/' directory from .gitignore to streamline ignored files for the project. This change ensures that only relevant build artifacts are excluded from version control.

---
+++ out/_next/static/chunks/21f17f7debb83a95.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/a7702373829cfe15.js",
+  "static/chunks/turbopack-ca411ac433b91247.js"
+])
---
+++ out/_next/static/chunks/4607b90505be9618.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/a7702373829cfe15.js",
+  "static/chunks/turbopack-6149eee475578b1f.js"
+])
